9905930825a64311757f55d6f6a18c0edd712418
Olivia Wong <oliwg138@outlook.com>
2018-10-04 15:03:13 +0800

    finished object array code

--- gpwk/exercism04/prime-factors.js
+++ gpwk/exercism04/prime-factors.js
@@ -1,20 +1,19 @@
-var PrimeFactors = function (number) {
-    this.number = number;
-};
 
 /*primeFactor is a class and for is a 
 function within this class*/ 
-primeFactors.prototype.for = function() {
+
+module.exports = {
+    for: function(number){
     var i=2;
     var solution=[];
-    while (number !== 1){ 
+    while (number !== 1){
+
         if(number%i === 0){
             number = number/i;
             solution.push(i);
-    } else {
-        i++;
-    }; 
-  }; return solution;
+        }   else {
+            i++;
+        };
+    };  return solution;
+  }
 };
-
-module.exports = PrimeFactors;
